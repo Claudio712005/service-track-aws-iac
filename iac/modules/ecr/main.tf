b2c42775818a348d4061e13acce11e8e2512eeba
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-07-25 18:54:08 -0300

    feat(ecr): repositorios por ambiente, lifecycle e tags imutaveis
    Adiciona lifecycle policy (mantem as ultimas N imagens tagueadas, expira
    untagged) e torna a imutabilidade da tag configuravel. Base para separar as
    imagens de HML e PRD com retencao independente.

--- iac/modules/ecr/main.tf
+++ iac/modules/ecr/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "this" {
   name                 = var.repository_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = var.image_tag_mutability
   force_delete         = true
 
   image_scanning_configuration {
@@ -8,4 +8,34 @@ resource "aws_ecr_repository" "this" {
   }
 
   tags = var.tags
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expira imagens sem tag apos ${var.untagged_expire_days} dias"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = var.untagged_expire_days
+        }
+        action = { type = "expire" }
+      },
+      {
+        rulePriority = 2
+        description  = "Mantem as ultimas ${var.max_image_count} imagens tagueadas"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = var.max_image_count
+        }
+        action = { type = "expire" }
+      },
+    ]
+  })
 }
--- iac/modules/ecr/variables.tf
+++ iac/modules/ecr/variables.tf
@@ -11,3 +11,23 @@ variable "scan_on_push" {
   type    = bool
   default = true
 }
+
+variable "image_tag_mutability" {
+  type    = string
+  default = "IMMUTABLE"
+
+  validation {
+    condition     = contains(["MUTABLE", "IMMUTABLE"], var.image_tag_mutability)
+    error_message = "image_tag_mutability deve ser MUTABLE ou IMMUTABLE."
+  }
+}
+
+variable "max_image_count" {
+  type    = number
+  default = 10
+}
+
+variable "untagged_expire_days" {
+  type    = number
+  default = 7
+}
