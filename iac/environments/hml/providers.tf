990b197acf96b459cc03e128d4018019bb971da9
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-08-11 22:39:56 -0300

    fix: token do EKS expirava antes do Helm rodar
    aws_eks_cluster_auth gera token de 15 min, resolvido no inicio do apply; criar
    cluster e node group leva mais que isso. Troca por exec, que busca o token na hora.

--- iac/environments/hml/providers.tf
+++ iac/environments/hml/providers.tf
@@ -2,20 +2,26 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_eks_cluster_auth" "this" {
-  name = module.stack.cluster_name
-}
-
 provider "kubernetes" {
   host                   = module.stack.cluster_endpoint
   cluster_ca_certificate = base64decode(module.stack.cluster_ca_data)
-  token                  = data.aws_eks_cluster_auth.this.token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.stack.cluster_name, "--region", var.aws_region]
+  }
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.stack.cluster_endpoint
     cluster_ca_certificate = base64decode(module.stack.cluster_ca_data)
-    token                  = data.aws_eks_cluster_auth.this.token
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.stack.cluster_name, "--region", var.aws_region]
+    }
   }
 }
